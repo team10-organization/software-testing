edf8d5e099bb2664c529a85c103dacddc6a97a5c
MaSungHo <52434222+MaSungHo@users.noreply.github.com>
2019-07-11 14:41:17 +0900

    CustomCalculatorTest (#2)
    * Add the add-test
    
    * Add the subtract-test
    
    * Add the multiply-test
    
    * Add the divide-test

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -13,35 +13,39 @@ public class CustomCalculatorTest {
     @Test
     public void add() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.add(10,15);
+        int result = customCalculator.add(473,684);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertThat(result, is(1157)); // changed by 마성호
+        System.out.println("The result is " + result);
     }
 
     //빼기 테스트 작성
     @Test
     public void subtract() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.subtract(23,10);
+        int result = customCalculator.subtract(65,28);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertTrue(result == 37); // changed by 마성호
+        System.out.println("The result is " + result);
     }
 
     //곱하기 테스트 작성
     @Test
     public void multiply() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.multiply(5,9);
+        int result = customCalculator.multiply(67, 645);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertThat(result, is(43215)); // changed by 마성호
+        System.out.println("The result is " + result);
     }
 
     //나누기 테스트 작성
     @Test
     public void divide() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.divide(25,5);
+        int result = customCalculator.divide(169,13);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertTrue(result == 13); //changed by 마성호
+        System.out.println("The result is " + result);
     }
 }
