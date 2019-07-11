b3279c60568035c5c1a99247861abe4f0ada48eb
MaSungHo <tonem123@naver.com>
2019-07-11 14:07:48 +0900

    Add the subtract-test

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -15,17 +15,18 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(473,684);
         /* 이곳에 테스트 코드를 작성하세요. */
-        assertThat(result, is(1157));
-        System.out.println("result :: " + result);
+        assertThat(result, is(1157)); // changed by 마성호
+        System.out.println("The result is " + result);
     }
 
     //빼기 테스트 작성
     @Test
     public void subtract() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.subtract(23,10);
+        int result = customCalculator.subtract(65,28);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertTrue(result == 37); // changed by 마성호
+        System.out.println("The result is " + result);
     }
 
     //곱하기 테스트 작성
